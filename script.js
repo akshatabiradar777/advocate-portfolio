e87ffedb8a1ffa1e06c91ec3ca421cd6bc85ce53
akshatabiradar777 <akshatabiradar777@gmail.com>
2026-03-28 17:19:46 +0530

    added advocate portfolio files
    uploaded HTNL,CSS,JS and images for portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+window.onload = function () {
+    alert("Welcome to Advocate Anil M Dodamani Portfolio");
+};
+
+// WhatsApp button function
+function contactWhatsApp() {
+    window.open("https://wa.me/918296016445", "_blank");
+}
